aff56833831f19ae692b07f87f59e41ed74f4458
muhammad-rajib <rajibhossain8521@gmail.com>
2024-03-08 22:20:56 +0600

    added: - request handler function added in OctopusAPI class

--- py_octopus/api.py
+++ py_octopus/api.py
@@ -7,7 +7,14 @@ class OctopusAPI:
     def __call__(self, environ, start_response):
         request = Request(environ)
 
-        response = Response()
-        response.text = "Hello Py-Octopus !!!"
+        response = self.handle_request(request)
 
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        user_agent = request.environ.get('HTTP_USER_AGENT', 'No User Agent Found')
+
+        response = Response()
+        response.text = f"Py-Octopus - User Agent: {user_agent}"
+        
+        return response
