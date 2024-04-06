6844cf2435883ef77c9c76697d3fbcb593df95a2
muhammad-rajib <rajibhossain8521@gmail.com>
2024-04-06 13:18:00 +0600

    Added: - Request handlers - Simple and parameterized routing New Package: - parse package added to handle parameterized routes

--- py_octopus/app.py
+++ py_octopus/app.py
@@ -8,9 +8,20 @@ app = OctopusAPI()
 
 @app.route("/home")
 def home(request, response):
-    response.text = "Hello from the HOME page"
+    response.text = f"Py-Octopus: The Fastest Web Application Framework"
+
+
+@app.route("/home/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello, {name}"
 
 
 @app.route("/about")
 def about(request, response):
     response.text = "Hello from the ABOUT page"
+
+
+@app.route("/add/{val_1:d}/{val_2:d}")
+def add(request, response, val_1, val_2):
+    total = int(val_1) + int(val_2)
+    response.text = f"{val_1} + {val_2} = {total}"
